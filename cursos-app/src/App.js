e415154f548b64f36da305fd183ff101cef73681
jorge22l <jorgeluismreyes@gmail.com>
2021-11-30 21:06:51 -0600

    Agregando AppBar

--- cursos-app/src/App.js
+++ cursos-app/src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import { TextField, ThemeProvider } from "@mui/material";
+import { Grid, ThemeProvider } from "@mui/material";
 import theme from './theme/Theme';
 import RegistrarUsuario from './components/seguridad/RegistrarUsuario';
 import PerfilUsuario from './components/seguridad/PerfilUsuario';
 import Login from './components/seguridad/Login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AppNavBar from './components/navegacion/AppNavBar';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Login />
-    </ThemeProvider>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <Grid container>
+          <AppNavBar>
+            <Routes>
+              
+              <Route exact path='/auth/login' element={<Login />} />
+              <Route exact path='/auth/registrar' element={ <RegistrarUsuario />} />
+              <Route exact path='/auth/perfil' element={ <PerfilUsuario />} />
+              <Route exact path="/" element={ <Login />} />
+            </Routes>
+            </AppNavBar>
+          </Grid>
+        </ThemeProvider>
+      </Router>
   );
 }
 
